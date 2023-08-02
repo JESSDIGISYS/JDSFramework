84d60351973f57b27987415cb24bcd50bec9513c
PHPDev7313 <61154728+PHPDev7313@users.noreply.github.com>
2023-08-02 15:08:37 -0400

    Cosmetic changes to the code in AbstractController that does not affect its performance - my preference

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -15,7 +15,6 @@ use Psr\Container\ContainerInterface;
 abstract class AbstractController
 {
 
-
 	protected ?ContainerInterface $container = null;
 
 	public function setContainer(ContainerInterface $container) : void
