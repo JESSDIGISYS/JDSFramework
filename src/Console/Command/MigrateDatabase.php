60bbe8b52a2695e5307c000236d14bea4479ca6f
PHPDev7313 <61154728+PHPDev7313@users.noreply.github.com>
2023-08-05 21:52:03 -0400

    Testing Commands

--- src/Console/Command/MigrateDatabase.php
+++ src/Console/Command/MigrateDatabase.php
@@ -5,8 +5,8 @@ namespace JDS\Console\Command;
 class MigrateDatabase implements CommandInterface
 {
 
-	public string $name = 'database:migrations:migrate';
-	
+	private string $name = 'database:migrations:migrate';
+
 	public function execute(array $params = []) : int
 	{
 		echo 'Executing MigrateDatabase command' . PHP_EOL;
--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace JDS\Console;
 
+use JDS\Console\Command\CommandInterface;
 use Psr\Container\ContainerInterface;
 
 final class Kernel {
@@ -42,12 +43,16 @@ final class Kernel {
 
 			// get the Command class name..using psr4 this will be same as filename
 			$command = $namespace.pathinfo($commandFile, PATHINFO_FILENAME);
-			dd($command);
 			// if it is a subclass of CommandInterface
+			if (is_subclass_of($command, CommandInterface::class))
+			{
 
 				// add to the container, using the name as the ID e.g. $container->add('database:migrations:migrate', MigrateDatabase::class)
-
+				$commandName = (new \ReflectionClass($command))->getProperty('name')->getDefaultValue();
+				dd($commandName);
+			}
 		}
+		
 		
 		// === register all user-defined commands (@todo) ===
 
