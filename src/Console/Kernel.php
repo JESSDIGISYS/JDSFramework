24bdd391ee3a53996ea1082e70c159650e30d73f
PHPDev7313 <61154728+PHPDev7313@users.noreply.github.com>
2023-08-05 21:42:25 -0400

    Testing Commands

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -2,7 +2,14 @@
 
 namespace JDS\Console;
 
+use Psr\Container\ContainerInterface;
+
 final class Kernel {
+
+		public function __construct(private ContainerInterface $container)
+		{
+
+		}
 
 	public function handle(): int 
 	{
@@ -22,16 +29,26 @@ final class Kernel {
 		// === register All built-in Commands ===
 		// get all files in the Commands dir
 		$commandFiles = new \DirectoryIterator(__DIR__ . '/Command');
-		dd($commandFiles);
-		
+
+		$namespace = $this->container->get('base-commands-namespace');
+				
 		// loop over all files in the commands folder
+		foreach ($commandFiles as $commandFile) {
+
+			// check if file is a dir
+			if (!$commandFile->isFile()) {
+				continue;
+			}
 
 			// get the Command class name..using psr4 this will be same as filename
-
+			$command = $namespace.pathinfo($commandFile, PATHINFO_FILENAME);
+			dd($command);
 			// if it is a subclass of CommandInterface
 
 				// add to the container, using the name as the ID e.g. $container->add('database:migrations:migrate', MigrateDatabase::class)
 
+		}
+		
 		// === register all user-defined commands (@todo) ===
 
 	}
