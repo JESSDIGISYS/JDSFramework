f439928c8d3619acf7efc543cda1242102ab5b26
PHPDev7313 <61154728+PHPDev7313@users.noreply.github.com>
2023-08-10 17:46:26 -0400

    Working on Console Application

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -7,6 +7,19 @@ class Application
 
 	public function run(): int
 	{
+		// use environment variables to obtain the command name
+		$argv = $_SERVER['argv'];
+		dd($argv);
+
+		// throw an exception if no command name is provided
+
+		// use command name to obtain a command object from the container
+
+		// parse variables to obtain options and args
+
+		// execute the command, returning the status code
+
+		// return the status code
 		return 0;
 	}
 }
