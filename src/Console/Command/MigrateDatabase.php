0e96c6cdcc78fa122a6e0964aa621d672a42752b
PHPDev7313 <61154728+PHPDev7313@users.noreply.github.com>
2023-08-10 20:24:23 -0400

    Working on Migration Database

--- src/Console/Command/MigrateDatabase.php
+++ src/Console/Command/MigrateDatabase.php
@@ -5,6 +5,7 @@ namespace JDS\Console\Command;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\DBAL\Schema\Schema;
+use Throwable;
 
 class MigrateDatabase implements CommandInterface
 {
@@ -18,27 +19,36 @@ class MigrateDatabase implements CommandInterface
 	public function execute(array $params = []): int
 	{
 
-		// create a migrations table sql if table not already in existence
-		$this->createMigrationsTable();
+		try {
+			// create a migrations table sql if table not already in existence
+			$this->createMigrationsTable();
 
-		// get $appliedMigrations which are already in the database.migrations table
+			// start a transaction
+			$this->connection->beginTransaction();
 
-		// get the $migrationFiles from the migrations folder
+			// get $appliedMigrations which are already in the database.migrations table
+			$appliedMigrations = $this->getAppliedMigrations();
+			// get the $migrationFiles from the migrations folder
 
-		// get the migrations to apply. i.e. they are in $migrationFiles but not in $appliedMigrations
+			// get the migrations to apply. i.e. they are in $migrationFiles but not in $appliedMigrations
 
-		// create sql for any migrations which have not been run ..i.e. which are not in the database
+			// create sql for any migrations which have not been run ..i.e. which are not in the database
 
-		// add migration to database
+			// add migration to database
 
-		// execute the sql query
+			// execute the sql query
+			$this->connection->commit();
 
 
 
+			echo 'Executing MigrateDatabase command' . PHP_EOL;
 
-		echo 'Executing MigrateDatabase command' . PHP_EOL;
+			return 0;
+		} catch (Throwable $throwable) {
+			$this->connection->rollBack();
 
-		return 0;
+			throw $throwable;
+		}
 	}
 
 	private function createMigrationsTable(): void
@@ -55,9 +65,14 @@ class MigrateDatabase implements CommandInterface
 			$sqlArray = $schema->toSql($this->connection->getDatabasePlatform());
 			$this->connection->executeQuery($sqlArray[0]);
 			echo 'Migrations table created' . PHP_EOL;
-		} else {
-			echo 'Migration table already exists' . PHP_EOL;
 		}
+	}
 
+	private function getAppliedMigrations(): array
+	{
+		$sql = "SELECT migration FROM migrations";
+		$appliedMigrations = $this->connection->executeQuery($sql)->fetchFirstColumn();
+		dd($appliedMigrations);
+		return $appliedMigrations;
 	}
 }
