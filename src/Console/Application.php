89f95aca9fbf76aa1ffa2450050f591ecd30d561
PHPDev7313 <61154728+PHPDev7313@users.noreply.github.com>
2023-08-15 09:43:22 -0400

    Adding rollback for databases

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -2,7 +2,7 @@
 
 namespace JDS\Console;
 
-use JDS\Container\Container;
+use League\Container\Container;
 use JDS\Exceptions\ConsoleException;
 
 
--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -3,8 +3,9 @@
 namespace JDS\Console;
 
 use JDS\Console\Application;
+
+use League\Container\Container;
 use JDS\Console\Command\CommandInterface;
-use JDS\Container\Container;
 
 final class Kernel {
 
@@ -50,7 +51,7 @@ final class Kernel {
 
 			// get the Command class name..using psr4 this will be same as filename
 			$command = $namespace.pathinfo($commandFile, PATHINFO_FILENAME);
-
+			
 			// if it is a subclass of CommandInterface
 			if (is_subclass_of($command, CommandInterface::class))
 			{
